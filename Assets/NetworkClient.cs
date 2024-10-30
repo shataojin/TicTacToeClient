af47ebd244e8be14f73a09f05b231dcf8f741c9a
shataojin <stj2062287@gmail.com>
2024-10-30 06:34:16 -0400

    Initial commit

---
+++ Assets/NetworkClient.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using Unity.Collections;
+using Unity.Networking.Transport;
+using System.Text;
+
+public class NetworkClient : MonoBehaviour
+{
+    NetworkDriver networkDriver;
+    NetworkConnection networkConnection;
+    NetworkPipeline reliableAndInOrderPipeline;
+    NetworkPipeline nonReliableNotInOrderedPipeline;
+    const ushort NetworkPort = 9001;
+    const string IPAddress = "10.0.0.97";
+
+    void Start()
+    {
+        networkDriver = NetworkDriver.Create();
+        reliableAndInOrderPipeline = networkDriver.CreatePipeline(typeof(FragmentationPipelineStage), typeof(ReliableSequencedPipelineStage));
+        nonReliableNotInOrderedPipeline = networkDriver.CreatePipeline(typeof(FragmentationPipelineStage));
+        networkConnection = default(NetworkConnection);
+        NetworkEndpoint endpoint = NetworkEndpoint.Parse(IPAddress, NetworkPort, NetworkFamily.Ipv4);
+        networkConnection = networkDriver.Connect(endpoint);
+    }
+
+    public void OnDestroy()
+    {
+        networkConnection.Disconnect(networkDriver);
+        networkConnection = default(NetworkConnection);
+        networkDriver.Dispose();
+    }
+
+    void Update()
+    {
+        #region Check Input and Send Msg
+
+        if (Input.GetKeyDown(KeyCode.A))
+            SendMessageToServer("Hello server's world, sincerely your network client");
+
+        #endregion
+
+        networkDriver.ScheduleUpdate().Complete();
+
+        #region Check for client to server connection
+
+        if (!networkConnection.IsCreated)
+        {
+            Debug.Log("Client is unable to connect to server");
+            return;
+        }
+
+        #endregion
+
+        #region Manage Network Events
+
+        NetworkEvent.Type networkEventType;
+        DataStreamReader streamReader;
+        NetworkPipeline pipelineUsedToSendEvent;
+
+        while (PopNetworkEventAndCheckForData(out networkEventType, out streamReader, out pipelineUsedToSendEvent))
+        {
+            if (pipelineUsedToSendEvent == reliableAndInOrderPipeline)
+                Debug.Log("Network event from: reliableAndInOrderPipeline");
+            else if (pipelineUsedToSendEvent == nonReliableNotInOrderedPipeline)
+                Debug.Log("Network event from: nonReliableNotInOrderedPipeline");
+
+            switch (networkEventType)
+            {
+                case NetworkEvent.Type.Connect:
+                    Debug.Log("We are now connected to the server");
+                    break;
+                case NetworkEvent.Type.Data:
+                    int sizeOfDataBuffer = streamReader.ReadInt();
+                    NativeArray<byte> buffer = new NativeArray<byte>(sizeOfDataBuffer, Allocator.Persistent);
+                    streamReader.ReadBytes(buffer);
+                    byte[] byteBuffer = buffer.ToArray();
+                    string msg = Encoding.Unicode.GetString(byteBuffer);
+                    ProcessReceivedMsg(msg);
+                    buffer.Dispose();
+                    break;
+                case NetworkEvent.Type.Disconnect:
+                    Debug.Log("Client has disconnected from server");
+                    networkConnection = default(NetworkConnection);
+                    break;
+            }
+        }
+
+        #endregion
+    }
+
+    private bool PopNetworkEventAndCheckForData(out NetworkEvent.Type networkEventType, out DataStreamReader streamReader, out NetworkPipeline pipelineUsedToSendEvent)
+    {
+        networkEventType = networkConnection.PopEvent(networkDriver, out streamReader, out pipelineUsedToSendEvent);
+
+        if (networkEventType == NetworkEvent.Type.Empty)
+            return false;
+        return true;
+    }
+
+    private void ProcessReceivedMsg(string msg)
+    {
+        Debug.Log("Msg received = " + msg);
+    }
+
+    public void SendMessageToServer(string msg)
+    {
+        byte[] msgAsByteArray = Encoding.Unicode.GetBytes(msg);
+        NativeArray<byte> buffer = new NativeArray<byte>(msgAsByteArray, Allocator.Persistent);
+
+        DataStreamWriter streamWriter;
+        networkDriver.BeginSend(reliableAndInOrderPipeline, networkConnection, out streamWriter);
+        streamWriter.WriteInt(buffer.Length);
+        streamWriter.WriteBytes(buffer);
+        networkDriver.EndSend(streamWriter);
+
+        buffer.Dispose();
+    }
+
+}
+
